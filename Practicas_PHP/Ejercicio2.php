6f32698fe5ddcdb76126222f8582ec8e9ee773f5
Julen Huarte <huarte.133745@e.unavarra.es>
2023-06-01 14:51:41 +0200

    Ejercicios PHP: Ejercicio 2 - Terminado

---
+++ Practicas_PHP/Ejercicio2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ejercicio 2</title>
+    </head>
+    <body>
+        <h1>Ejercicio 2</h1>
+        <?php
+            for($i = 0; $i < 10; $i = $i+1) 
+            {
+                echo "Hola mundo!!!<br>";
+                echo "Bienvenidos al php!!!<br>";
+            }
+        ?>
+    </body>
+</html>
